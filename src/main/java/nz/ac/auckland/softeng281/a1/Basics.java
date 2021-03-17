bdab18af420967456bbc7a64265cdfabab1bd91e
aahl907 <aahl907@aucklanduni.ac.nz>
2021-03-17 04:27:26 +0000

    completed task one (with full comments)

--- src/main/java/nz/ac/auckland/softeng281/a1/Basics.java
+++ src/main/java/nz/ac/auckland/softeng281/a1/Basics.java
@@ -14,13 +14,11 @@ public class Basics {
    * @return the new balance
    */
   public static double deposit(double currentBalance, double amount, int numOfTransactions, int transactionsLimit) {
-    // If numOfTransactions reached transactionsLimit, method returns the current
-    // balance unchanged and prints “transactions limit reached”, otherwise the
-    // method returns new current balance.
+    //If numOfTransactions reached transactionsLimit, method returns the current balance unchanged and prints “transactions limit reached".
     if (numOfTransactions == transactionsLimit) {
       System.out.println("transactions limit reached");
       return currentBalance;
-    } else {
+    } else { //otherwise the method returns new current balance.
       currentBalance += amount;
       return currentBalance;
     }
@@ -37,13 +35,14 @@ public class Basics {
    * @return the new balance
    */
   public static double withdraw(double currentBalance, double amount, int numOfTransactions, int transactionsLimit) {
+    //If numOfTransactions reached transactionsLimit, method returns the current balance unchanged and prints “transactions limit reached".
     if (numOfTransactions == transactionsLimit) {
       System.out.println("transactions limit reached");
       return currentBalance;
-    } else if (currentBalance >= amount) {
+    } else if (currentBalance >= amount) { //if the current balance sufficient, do transaction
       currentBalance -= amount;
       return currentBalance;
-    } else {
+    } else { //otherwise print "amount exceeded" and return unchanged balance.
       System.out.println("amount exceeded");
       return currentBalance;
     }
